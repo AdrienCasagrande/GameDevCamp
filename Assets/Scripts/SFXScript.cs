6b4e167b94042199c42f9f62e130c6fe1d89cb5e
casagr_a <casagr_a@etna-alternance.net>
2015-06-05 19:07:25 +0200

    more folders + explosion + sounds

---
+++ Assets/Scripts/SFXScript.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SFXScript : MonoBehaviour {
+
+	public static SFXScript instance;
+	public ParticleSystem boomEffect;
+	public AudioClip boomSound;
+
+	void Awake() {
+		if (instance != null) {
+			print ("more than one instance of SFXScript, or not");
+			Destroy (this);
+		} else {
+			instance = this;
+		}
+	}
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	public void explode(Vector3 position) {
+		playSound (boomSound);
+		instantiate (boomEffect, position);
+	}
+
+	private ParticleSystem instantiate(ParticleSystem prefab, Vector3 position) {
+		ParticleSystem particle = Instantiate (prefab, position, Quaternion.identity) as ParticleSystem;
+		Destroy (particle.gameObject, particle.startLifetime);
+		return particle;
+	}
+
+	public void playSound(AudioClip sound) {
+		AudioSource.PlayClipAtPoint (sound, transform.position);
+	}
+}
