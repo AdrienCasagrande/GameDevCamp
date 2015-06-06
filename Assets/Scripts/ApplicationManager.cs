07c6e302bffb779ab256101099a3339df5e1f35a
Mahefa Andrianifahanana <nmahefa@gmail.com>
2015-06-06 18:16:29 +0200

    ajout du menu

--- Assets/Scripts/ApplicationManager.cs
+++ Assets/Scripts/ApplicationManager.cs
@@ -12,4 +12,19 @@ public class ApplicationManager : MonoBehaviour {
 		Application.Quit();
 		#endif
 	}
+
+	public void Play()
+	{
+		FadeLevel.LoadLevel("FirstScene" , 1,1,Color.black);
+	}
+
+	public void Main()
+	{
+		FadeLevel.LoadLevel ("Menu 3D", 1, 1, Color.black);
+	}
+
+	public void Over()
+	{
+		FadeLevel.LoadLevel ("GameOver", 1, 1, Color.black);
+	}
 }
