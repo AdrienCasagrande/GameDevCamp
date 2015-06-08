bc728f0b8bee8d832d614e808c74dbc2c0f3b48e
casagr_a <casagr_a@etna-alternance.net>
2015-06-08 14:26:33 +0200

    pre alpha 0.0.1

---
+++ Assets/Scripts/Garbage.cs
@@ -0,0 +1,50 @@
+using UnityEngine;
+using System.Collections;
+
+public class Garbage : MonoBehaviour
+{
+
+	public GameObject[] prefab;
+
+	private int size_ = 1;
+	private int type;
+	private bool is_alive_;
+
+
+	public bool isAlive() {
+		return is_alive_;
+	}
+
+	public void SetSize(int size) {
+		size_ = size;
+	}
+
+	// Use this for initialization
+	void Start ()
+	{
+		is_alive_ = true;
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+	
+	}
+
+	void OnCollisionEnter2D(Collision2D c) {
+		int score = 0;
+		switch (size_)
+		{
+		case 1:
+			score = 100;
+			break;
+		case 2:
+			score = 50;
+			break;
+		case 3:
+			score = 20;
+			break;
+		}
+	}
+}
+
--- Assets/Scripts/Spawner.cs
+++ Assets/Scripts/Spawner.cs
@@ -6,23 +6,47 @@ public class Spawner : MonoBehaviour {
 	public float spawnTime = 5f;		// The amount of time between each spawn.
 	public float spawnDelay = 3f;		// The amount of time before spawning starts.
 	public GameObject[] enemies;		// Array of enemy prefabs.
+	public float forceMax = 1;
+	public float forceMin = 5;
+	private float angle;
+//	private Camera camera;
+	Vector2 direction;
 
-	
 	void Start ()
 	{
 		// Start calling the Spawn function repeatedly after a delay .
 		// Invoke("Spawn", spawnTime);
+	
 	}
 
-	void InvokeSpawn(float spawnDelay) {
-		Invoke("Spawn", spawnTime);
-	}
-
-	void Spawn ()
+	GameObject Spawn ()
 	{
 		// Instantiate a random enemy.
 		int enemyIndex = Random.Range(0, enemies.Length);
+
+		Vector3 target = Camera.main.ViewportToScreenPoint(new Vector3(0.5f, 0.5f, 1.0f));
+		angle = Mathf.Atan2(target.y - transform.position.y, target.x - transform.position.x) * Mathf.Rad2Deg;
+		transform.rotation = Quaternion.AngleAxis(angle - 90, Vector3.forward);
+
 		GameObject clone = Instantiate(enemies[enemyIndex], transform.position, transform.rotation) as GameObject;
-		clone.GetComponent<Rigidbody2D>().AddForce (transform.up * 100);
+
+		switch (Random.Range (1,4)) {
+			case 1:
+				direction = - transform.up;
+				break;
+			case 2:
+				direction = - transform.right;
+				break;
+			case 3:	
+				direction = transform.up;
+				break;
+			case 4:
+				direction = transform.right;
+				break;
+			default:
+				break;
+		}
+		clone.GetComponent<Rigidbody2D>().AddForce (direction * Random.Range(forceMin, forceMax)/5, ForceMode2D.Impulse);
+		return clone;
 	}
 }
